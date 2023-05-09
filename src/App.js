d822d0f8e572505ac04daf906b8c6c6900c2cfd5
Omer <notomerkhan@gmail.com>
2023-05-09 04:07:56 -0700

    Added fonts and image and basic text features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1 style={{ fontSize: '100px',  marginTop: '-5px' }}>My Portfolio</h1>
+        <omer-header style={{ fontSize: '60px' }}>- Omer's Portfolio.</omer-header>
+        <a href="#card1">Card 1</a>
+        <a href="#card2">Card 2</a>
+        <a href="#card3">Card 3</a>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          <div className="col s12 m6 l4">
+            <div className="card" id="card1">
+              <h2>Card 1</h2>
+              <body-text>This is a sample card. You can add more cards like this to your page.</body-text>
+            </div>
+          </div>
+          <div className="col s12 m6 l4">
+            <div className="card" id="card2">
+              <h2>Card 2</h2>
+              <body-text>This is a sample card. You can add more cards like this to your page.</body-text>
+            </div>
+          </div>
+          <div className="col s12 m6 l4">
+            <div className="card" id="card3">
+              <h2>Card 3</h2>
+              <body-text>This is a sample card. You can add more cards like this to your page.</body-text>
+            </div>
+          </div>
+        </div>
+      </div>
+      <body-text style={{ fontSize: '8px' }}>- Allhumdulilah for everything ❤️</body-text>
     </div>
   );
 }
---
+++ src/socials.js
@@ -0,0 +1,9 @@
+<div class="container">
+  <img src="./images/iphone-home-screen.png" alt="iPhone Home Screen"> </img>
+  <div class="icons">
+    <a href="#" class="icon-link"><i class="fas fa-camera"></i></a>
+    <a href="#" class="icon-link"><i class="fas fa-music"></i></a>
+    <a href="#" class="icon-link"><i class="fas fa-envelope"></i></a>
+    ...
+  </div>
+</div>
