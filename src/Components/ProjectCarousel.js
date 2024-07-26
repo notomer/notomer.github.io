a7a528b4566313fa0d8d095cbd3c3a8f17c3ce74
Omer Khan <notomerkhan@gmail.com>
2024-07-26 12:00:35 -0400

    Resume

--- src/Components/ProjectCarousel.js
+++ src/Components/ProjectCarousel.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../Slideshow.css"; 
+import "../Slideshow.css";
 
 const ProjectCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -15,13 +15,13 @@ const ProjectCarousel = () => {
       filename: "images/apple.png",
       description: "My Work at Apple",
       paragraph:
-        "At Apple, I worked on the cloud build platform, streamlining deployment and delivery. I had developed 3 main tools. Firstly, a clang & compiler error extraction tool, that automatically detects and finds build warnings and errors and emails the code author based on their compilation flags and build requirements. Second, I converted a command line tool originally which required complicated installation steps and setup to be deployed over CLI without the need of additional dependencies by using a trampoline command and self-contained dependency resolution. Lastly, i had begun the development of automated AddressSanitizer (aka ASan) installation, ASan is a memory error detector. These roots for iOS builds originally needed to be manually installed, per iOS version, this tool aimed to eliminate that.",
+        "At Apple, I worked on the cloud build platform, streamlining deployment and delivery. I developed three main tools. Firstly, a Clang and compiler error extraction tool that automatically detects and finds build warnings and errors and emails the code author based on their compilation flags and build requirements. Secondly, I converted a command-line tool, which originally required complicated installation steps and setup, to be deployed over CLI without the need for additional dependencies by using a trampoline command and self-contained dependency resolution. Lastly, I began the development of automated AddressSanitizer (ASan) installation. ASan is a memory error detector, and these roots for iOS builds originally needed to be manually installed per iOS version. This tool aimed to eliminate that."
     },
     {
       filename: "images/vision.png",
       description: "Machine Learning Work",
       paragraph:
-        "This next chapter I want to focus on Machine Learning. Right now, I've been looking at using coreML Swift framework in my applications. I've also been taking a look at using ML at the gym, my biggest concern is my from being correct, I was inspired by the PoseNet model and am currently researching a development of my own model",
+        "This next chapter, I want to focus on Machine Learning. Currently, I’ve been exploring the use of the CoreML Swift framework in my applications. Additionally, I’ve been investigating the development of an action model for complete computer automation."
     },
   ];
 
@@ -31,7 +31,7 @@ const ProjectCarousel = () => {
       return newIndex;
     });
   };
-  
+
   const handleNext = () => {
     setCurrentIndex((prevIndex) => {
       const newIndex = prevIndex === images.length - 1 ? 0 : prevIndex + 1;
@@ -41,7 +41,7 @@ const ProjectCarousel = () => {
 
 
   console.log("currentIndex before return:", currentIndex);
-  
+
 
   return (
     <div className="image-slider">
@@ -49,7 +49,7 @@ const ProjectCarousel = () => {
         <img src="/images/arrowL.png" alt="Previous" />
       </button>
       <div className={`slider-container ${currentIndex === 0 ? 'active' : ''}`}>
-      {console.log("Rendered with currentIndex:", currentIndex)}
+        {console.log("Rendered with currentIndex:", currentIndex)}
         <img
           className="slide-image"
           src={`/${images[currentIndex].filename}`}
--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EmailEncrypt from './emailencrypt';
+import ResumeModel from './ResumeModel'; // Correct import
+
 const SocialLinks = () => {
+  const [isModelOpen, setIsModelOpen] = useState(false);
+
+  const openModel = () => {
+    setIsModelOpen(true);
+  };
+
+  const closeModel = () => {
+    setIsModelOpen(false);
+  };
+
   return (
     <div className="ContactLinks">
       {/* LinkedIn */}
@@ -19,6 +31,15 @@ const SocialLinks = () => {
 
       {/* EmailEncrypt */}
       <EmailEncrypt className="classic-tab" target="_blank" rel="noopener noreferrer" id="email-link" />
+
+      {/* Resume */}
+      <button className="classic-tab" onClick={openModel} id="resume-button">
+        <img src="/images/Pages.webp" alt="Resume" className="icon" />
+        Resume
+        <div className="gloss-layer"></div>
+      </button>
+
+      {isModelOpen && <ResumeModel onClose={closeModel} />}
     </div>
   );
 };
